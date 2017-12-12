c577e8801e3e62fb4fef7192666235a319db5529
MerryMage <MerryMage@users.noreply.github.com>
2017-12-12 03:04:16 +0000

    travis/macos: Use clang 5.0

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -2,8 +2,16 @@
 
 set -o pipefail
 
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/llvm/include $CPPFLAGS"
+export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib $LDFLAGS"
+export CC="clang"
+export CXX="clang++"
+
 export MACOSX_DEPLOYMENT_TARGET=10.9
 export Qt5_DIR=$(brew --prefix)/opt/qt5
+
+$(CXX) --version
 
 mkdir build && cd build
 cmake .. -DUSE_SYSTEM_CURL=ON -DCMAKE_OSX_ARCHITECTURES="x86_64;x86_64h" -DCMAKE_BUILD_TYPE=Release
