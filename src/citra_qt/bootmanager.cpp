aed3a41f9ddc28ef0a26f71096fe8aba04f0f4b7
MerryMage <MerryMage@users.noreply.github.com>
2018-04-04 18:30:31 +0100

    fixup! bootmanager: Request minidump

--- src/citra_qt/bootmanager.cpp
+++ src/citra_qt/bootmanager.cpp
@@ -3,12 +3,12 @@
 #include <QDir>
 #include <QHBoxLayout>
 #include <QKeyEvent>
-#include <QStandardPaths>
 #include <QScreen>
 #include <QWindow>
 
 #include "citra_qt/bootmanager.h"
 #include "common/crash_handler.h"
+#include "common/file_util.h"
 #include "common/microprofile.h"
 #include "common/scm_rev.h"
 #include "common/string_util.h"
@@ -21,7 +21,7 @@
 #include "network/network.h"
 
 static std::string GenerateMinidumpFilename() {
-    QString dir = QStandardPaths::writableLocation(QStandardPaths::DesktopLocation);
+    QString dir = QString::fromStdString(FileUtil::GetUserPath(D_LOGS_IDX));
     QString file = QDateTime::currentDateTime().toString("'minidump-citra-'yyyyMMdd-HHmmss'.dmp'");
     return QDir::toNativeSeparators(dir + "/" + file).toStdString();
 }
